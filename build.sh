49e018ddef4584356c339dcb6c530ef558a5b17f
nedalturas527 <nedalturas527@gmail.com>
2024-11-03 09:13:29 +0000

    changed the directory in the sed command

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 cp js/config.template.js js/config.js
 
 # Replace the placeholders with environment variables
-sed -i "s/SHEET_ID_PLACEHOLDER/$GOOGLE_SHEETS_ID/g" config.js
-sed -i "s/API_KEY_PLACEHOLDER/$GOOGLE_SHEETS_API_KEY/g" config.js
+sed -i "s/SHEET_ID_PLACEHOLDER/$GOOGLE_SHEETS_ID/g" js/config.js
+sed -i "s/API_KEY_PLACEHOLDER/$GOOGLE_SHEETS_API_KEY/g" js/config.js
